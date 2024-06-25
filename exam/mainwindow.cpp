b210e8ddd6fecf71900868a9d8b9200db3e07d2d
Matthew <matveynikishaev@gmail.com>
2024-06-25 12:06:48 +0300

    init

---
+++ exam/mainwindow.cpp
@@ -0,0 +1,150 @@
+#include "mainwindow.h"
+#include <QRandomGenerator>
+#include <QMessageBox>
+#include <QBuffer>
+#include <QCryptographicHash>
+#include <QFile>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QJsonArray>
+
+#include <openssl/evp.h>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+
+    ui->stackedWidget->setCurrentWidget(ui->gamePage);
+
+    // connect(ui->loginButton, &QPushButton::clicked, this, &MainWindow::onLoginButtonClicked);
+    // connect(ui->resetButton, &QPushButton::clicked, this, &MainWindow::onResetButtonClicked);
+
+    // for (int i = 0; i < 9; ++i) {
+    //     QPushButton *cardButton = findChild<QPushButton*>(QString("card%1").arg(i + 1));
+    //     if (cardButton) {
+    //         connect(cardButton, &QPushButton::clicked, this, &MainWindow::onCardClicked);
+    //     }
+    // }
+
+    // resetGame();
+    // loadGame();
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+
+
+
+
+
+
+int MainWindow::decryptQByteArray(const QByteArray& encryptedBytes, QByteArray& decryptedBytes, unsigned char *key)
+{
+    qDebug() << "Decrypting";
+    QByteArray iv_hex("00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f");
+    QByteArray iv_ba = QByteArray::fromHex(iv_hex);
+
+    unsigned char iv[16] = {0};
+    memcpy(iv, iv_ba.data(), 16);
+
+    EVP_CIPHER_CTX *ctx;
+    ctx = EVP_CIPHER_CTX_new();
+    if (!EVP_DecryptInit_ex2(ctx, EVP_aes_256_cbc(), key, iv, NULL)) {
+        qDebug() << "Error";
+        /* Error */
+        EVP_CIPHER_CTX_free(ctx);
+        return 0;
+    }
+
+    #define BUF_LEN 256
+    unsigned char encrypted_buf[BUF_LEN] = {0}, decrypted_buf[BUF_LEN] = {0};
+    int encr_len, decr_len;
+
+    QDataStream encrypted_stream(encryptedBytes);
+
+    decryptedBytes.clear();
+    QBuffer decryptedBuffer(&decryptedBytes);
+    decryptedBuffer.open(QIODevice::ReadWrite);
+
+
+    encr_len = encrypted_stream.readRawData(reinterpret_cast<char*>(encrypted_buf), BUF_LEN);
+    while(encr_len > 0){
+
+        if (!EVP_DecryptUpdate(ctx, decrypted_buf, &decr_len, encrypted_buf, encr_len)) {
+            /* Error */
+            qDebug() << "Error";
+            EVP_CIPHER_CTX_free(ctx);
+            return 0;
+        }
+
+        decryptedBuffer.write(reinterpret_cast<char*>(decrypted_buf), decr_len);
+        encr_len = encrypted_stream.readRawData(reinterpret_cast<char*>(encrypted_buf), BUF_LEN);
+    }
+
+    int tmplen;
+    if (!EVP_DecryptFinal_ex(ctx, decrypted_buf, &tmplen)) {
+        /* Error */
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    decryptedBuffer.write(reinterpret_cast<char*>(decrypted_buf), tmplen);
+    EVP_CIPHER_CTX_free(ctx);
+
+    decryptedBuffer.close();
+    return 0;
+}
+
+int MainWindow::encryptQByteArray(const QByteArray &plainBytes, QByteArray &encryptedBytes, unsigned char *key)
+{
+    qDebug() << "Encrypting";
+
+    QByteArray iv_hex("00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f");
+    QByteArray iv_ba = QByteArray::fromHex(iv_hex);
+    unsigned char iv[16] = {0};
+    memcpy(iv, iv_ba.data(), 16);
+
+    EVP_CIPHER_CTX *ctx;
+    ctx = EVP_CIPHER_CTX_new();
+    if (!EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv)) {
+        qDebug() << "Error: EVP_EncryptInit_ex";
+        EVP_CIPHER_CTX_free(ctx);
+        return 0;
+    }
+
+    #define BUF_LEN 256
+    unsigned char encrypted_buf[BUF_LEN] = {0}, plain_buf[BUF_LEN] = {0};
+    int encr_len, plain_len;
+
+    QDataStream plain_stream(plainBytes);
+
+    encryptedBytes.clear();
+    QBuffer encryptedBuffer(&encryptedBytes);
+    encryptedBuffer.open(QIODevice::ReadWrite);
+
+    plain_len = plain_stream.readRawData(reinterpret_cast<char*>(plain_buf), BUF_LEN);
+    while (plain_len > 0) {
+        if (!EVP_EncryptUpdate(ctx, encrypted_buf, &encr_len, plain_buf, plain_len)) {
+            qDebug() << "Error: EVP_EncryptUpdate";
+            EVP_CIPHER_CTX_free(ctx);
+            return 0;
+        }
+
+        encryptedBuffer.write(reinterpret_cast<char*>(encrypted_buf), encr_len);
+        plain_len = plain_stream.readRawData(reinterpret_cast<char*>(plain_buf), BUF_LEN);
+    }
+
+    int tmplen;
+    if (!EVP_EncryptFinal_ex(ctx, encrypted_buf, &tmplen)) {
+        qDebug() << "Error: EVP_EncryptFinal_ex";
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    encryptedBuffer.write(reinterpret_cast<char*>(encrypted_buf), tmplen);
+    EVP_CIPHER_CTX_free(ctx);
+
+    encryptedBuffer.close();
+    return 0;
+}
---
+++ exam/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMap>
+#include <QVector>
+#include <QFile>
+#include "ui_mainwindow.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+
+private:
+    // QString jsonPath = "D:/Education/Education4/enc/exam/JSON/last_game.json";
+
+
+    Ui::MainWindow *ui;
+    int correctPinCode = 1234;
+
+
+
+
+    int decryptQByteArray(const QByteArray& encryptedBytes, QByteArray& decryptedBytes, unsigned char *key);
+    int encryptQByteArray(const QByteArray &plainBytes, QByteArray &encryptedBytes, unsigned char *key);
+};
+
+#endif // MAINWINDOW_H
